2fe133b6bda267d20e4abc68b2a0e9429e385a54
thanosa <athansios.anastasiadis@gmail.com>
2020-05-07 22:48:02 +0300

    Win 1st wooden 1

---
+++ codingame/competitions/02_pacman/bot.py
@@ -0,0 +1,71 @@
+import sys
+import math
+
+
+def manhattan(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+# width: size of the grid
+# height: top left corner is (x=0, y=0)
+width, height = [int(i) for i in input().split()]
+for i in range(height):
+    row = input()  # one line of the grid: space " " is floor, pound "#" is wall
+
+
+# game loop
+while True:
+    pacs_mine = []
+    pacs_their = []
+
+    my_score, opponent_score = [int(i) for i in input().split()]
+    visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
+    for i in range(visible_pac_count):
+        # pac_id: pac number (unique within a team)
+        # mine: true if this pac is yours
+        # x: position in the grid
+        # y: position in the grid
+        # type_id: unused in wood leagues
+        # speed_turns_left: unused in wood leagues
+        # ability_cooldown: unused in wood leagues
+        pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown = input().split()
+        pac_id = int(pac_id)
+        point = (int(x), int(y))
+
+        if mine == "1":
+            pacs_mine.append(point)
+        else:
+            pacs_their.append(point)
+
+        speed_turns_left = int(speed_turns_left)
+        ability_cooldown = int(ability_cooldown)
+    visible_pellet_count = int(input())  # all pellets in sight
+    
+    super_pellets = []
+    normal_pellets = []
+
+    for i in range(visible_pellet_count):
+        # value: amount of points this pellet is worth
+        x, y, value = [int(j) for j in input().split()]
+        if value == 10:
+            super_pellets.append((x, y))
+        else:
+            normal_pellets.append((x,y))
+    
+    for pac in pacs_mine:
+        if len(super_pellets) > 0:
+            target = super_pellets
+        else:
+            target = normal_pellets
+
+        min_distance = math.inf
+        for i, super_pellet in enumerate(target):
+            distance = manhattan(pac, super_pellet)
+            if distance <= min_distance:
+                min_distance = distance
+                index = i
+
+        print(f"MOVE 0 {target[index][0]} {target[index][1]}")
+
+
+    # MOVE <pacId> <x> <y>
+

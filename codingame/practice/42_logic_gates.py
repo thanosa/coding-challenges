1df9cc84ae6ba5413948720ac2bba74dc956022a
thanosa <thanos.a@tutanota.com>
2022-06-26 19:07:07 +0300

    Solve logic gates

--- codingame/practice/42_logic_gates.py
+++ codingame/practice/42_logic_gates.py
@@ -1,45 +1,28 @@
-'''
-
-https://www.codingame.com/ide/puzzle/logic-gates
-
-'''
-
 import numpy as np
-import sys
 
-def debug(msg):
-    print(msg, file=sys.stderr, flush=True) 
-
-
-def calc(_type: str, input_1: np.ndarray, input_2: np.ndarray) -> str:
+def calc(_type: str, input_1: np.ndarray, input_2: np.ndarray) -> np.ndarray:
     
-    result = []
+    invert = _type[0] == 'N'
+    operation = _type if not invert else _type[1:]
 
-    if _type == "AND":
-        result = np.bitwise_and(input_1, input_2)
-    elif _type == "OR":
-        result = np.bitwise_or(input_1, input_2)
-    elif _type == "XOR":
-        result = np.bitwise_xor(input_1, input_2)
-    elif _type == "NAND":
-        result = np.invert(np.bitwise_and(input_1, input_2))
-    elif _type == "NOR":
-        result = np.invert(np.bitwise_or(input_1, input_2))
-    elif _type == "NXOR":
-        result = np.invert(np.bitwise_xor(input_1, input_2))
+    f = {
+        "AND": np.bitwise_and,
+        "OR": np.bitwise_or,
+        "XOR": np.bitwise_xor
+    }
 
-    return ''.join(['-' if x else '_' for x in result])
+    result = f[operation](input_1, input_2)
+    return result if not invert else np.invert(result)
 
+n, m = int(input()), int(input())
 
-n = int(input())
-m = int(input())
 inputs = {}
 for i in range(n):
     input_name, input_signal = input().split()
     inputs[input_name] = np.array([x == '-' for x in input_signal])
 
-outputs = {}
 for i in range(m):
     output_name, _type, input_name_1, input_name_2 = input().split()
-    out = calc(_type, inputs[input_name_1], inputs[input_name_2])
-    print(f"{output_name} {out}")
+    result = calc(_type, inputs[input_name_1], inputs[input_name_2])
+    converted = ''.join(['-' if x else '_' for x in result])
+    print(f"{output_name} {converted}")
